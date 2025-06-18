515f24a0972a4a134b67a3b7f557edfead1fa4c6
Lilian <lil.bouhier@gmail.com>
2025-06-18 17:49:41 +0200

    ajout du mode 'jeu'

--- src/game.py
+++ src/game.py
@@ -54,3 +54,32 @@ class Menu():
         nb2_pos = nb2.get_rect(centerx = pos_x, top = gap_pos + 75)
         self.screen.blit(nb1, nb1_pos)
         self.screen.blit(nb2, nb2_pos)
+
+
+
+class Game():
+    def __init__(self, screen, largeur_screen, hauteur_screen):
+
+        self.screen = screen
+        self.largeur = largeur_screen
+        self.hauteur = hauteur_screen
+
+        self.background = pygame.image.load("../assets/img/background-day.png").convert_alpha()
+        self.background = pygame.transform.scale(self.background, (self.largeur, self.hauteur))
+
+        self.tuyau_img = pygame.image.load("../assets/img/pipe-green.png").convert_alpha()
+        self.tuyau_img = pygame.transform.scale(self.tuyau_img, (70, 600))
+
+        self.sol = pygame.image.load("../assets/img/base.png").convert_alpha()
+        self.sol = pygame.transform.scale(self.sol, (self.largeur, round(self.largeur / 3)))
+        self.sol_pos = self.sol.get_rect(x=0, bottom=self.hauteur)
+
+        self.score = 0
+        self.font = pygame.font.Font("../assets/font/Jersey15-Regular.ttf", 50)
+
+    def run(self):
+        self.screen.blit(self.background, (0, 0))
+        self.screen.blit(self.sol, self.sol_pos)
+        self.texte = self.font.render(f"{self.score}", True, blanc)
+        self.texte_pos = self.texte.get_rect(centerx=round(self.largeur / 2), centery=round(self.hauteur / 5))
+        self.screen.blit(self.texte, self.texte_pos)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import pygame
 from game import *
 pygame.init()
 
@@ -10,16 +9,21 @@ pygame.display.set_caption("Flappy bird")
 
 # Modes
 M_menu = Menu(screen, largeur, hauteur)
+M_game = Game(screen, largeur, hauteur)
+mode = M_menu
 
 clock = pygame.time.Clock()
 running_global = True
 while running_global:
 
-    M_menu.run()
+    mode.run()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running_global = False
+        elif event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_SPACE :
+                mode = M_game
 
     pygame.display.flip()
     clock.tick(30)
